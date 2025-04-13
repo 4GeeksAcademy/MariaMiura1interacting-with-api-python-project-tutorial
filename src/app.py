87597de8012b16b04578b3e1b335cf14cdc547b5
MariaMiura1 <166013572+MariaMiura1@users.noreply.github.com>
2025-04-13 11:05:17 +0000

    updated

--- src/app.py
+++ src/app.py
@@ -5,3 +5,8 @@ from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+CLIENT_ID = "e18698933cb347c9a343c8d25ec9616f"
+CLIENT_SECRET ="995e3dc1447f4236869c280c18049390"
+
+client_id = os.environ.get("e18698933cb347c9a343c8d25ec9616f")
+client_secret = os.environ.get("447f4236869c280c18049390")
